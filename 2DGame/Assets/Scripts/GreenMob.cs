2e9dfb6de5c65c2de201c4843764c2de5b085e26
Norbert <norbertskulski@wp.pl>
2019-11-03 14:14:50 +0100

    test gitignore

---
+++ 2DGame/Assets/Scripts/GreenMob.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenMob : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speedMob;
+    public float jumpHeight;
+    public Transform chitCheck;
+    public float ChitRadious;
+    public LayerMask Hit;
+    private bool hited;
+    private PolygonCollider2D Pc;
+    private bool stopHit;
+     
+    void Start()
+    {
+        Pc = GetComponent<PolygonCollider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hited&&!stopHit)
+        {
+            Destroy(Pc);
+            jump();
+            stopHit = true;
+          
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-speedMob, GetComponent<Rigidbody2D>().velocity.y);
+        hited = Physics2D.OverlapCircle(chitCheck.position, ChitRadious, Hit);
+    }
+
+    public void jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Ground")
+        {
+            Debug.Log("Colision detected");
+            speedMob = speedMob * (-1);
+        }
+    }
+}
---
+++ 2DGame/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float moveSpeed;
+    public float jumpHeight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask IsGround;
+    private bool grounded;
+    private bool doublejump;
+
+
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      
+        if (grounded&&Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump();
+            doublejump = true;
+        };
+
+        if (doublejump&&!grounded && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump();
+            doublejump = false;
+        };
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow)||Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, IsGround);
+    }
+
+    public void jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+    }
+}
